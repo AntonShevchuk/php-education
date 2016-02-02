aa22a1132b389645aa86963515f9152b2910363f
Anton Shevchuk <AntonShevchuk@gmail.com>
2016-02-02 13:03:09 +0200

    Added session article

---
+++ server.php
@@ -0,0 +1,6 @@
+<?php
+if (file_exists(__DIR__ . $_SERVER['SCRIPT_NAME'])) {
+    return false; // serve the requested resource as-is.
+} else {
+    include_once 'rewrite.php';
+}
---
+++ session.handler.php
@@ -0,0 +1,3 @@
+<?php
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
