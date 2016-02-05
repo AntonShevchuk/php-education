d958ffbbddbeb7e07fc71c5e1f12f2a7a1b429f1
Anton Shevchuk <AntonShevchuk@gmail.com>
2016-02-05 18:54:55 +0200

    More text

--- session.start.php
+++ session.start.php
@@ -4,5 +4,26 @@ ini_set('display_errors', 1);
 
 session_start();
 
-echo $_SESSION['id'];
-$_SESSION['id'] = 42;
+if (!isset($_SESSION['time'])) {
+    $_SESSION['ua'] = $_SERVER['HTTP_USER_AGENT'];
+    $_SESSION['time'] = date("H:i:s");
+}
+
+if ($_SESSION['ua'] != $_SERVER['HTTP_USER_AGENT']) {
+    /**
+     * @link http://php.net/function.session-destroy
+     */
+
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+    // Finally, destroy the session.
+    session_destroy();
+    die('Wrong browser');
+}
+
+echo $_SESSION['time'];
