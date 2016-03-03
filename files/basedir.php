aa244135d3855cd0a7e5ca4acf4231d94d79aacc
AntonShevchuk <AntonShevchuk@gmail.com>
2016-03-03 19:32:48 +0200

    Updated example with basedir

--- files/basedir.php
+++ files/basedir.php
@@ -3,9 +3,9 @@ error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 
 echo 'basedir: '. ini_get('open_basedir') .'<br/>';
-echo 'filesize: ../phpinfo.php - '. filesize('../phpinfo.php') . '<br/>';
+echo 'filesize: ../phpinfo.php - '. filesize(__DIR__ .'/../phpinfo.php') . '<br/>';
 
 ini_set('open_basedir', __DIR__);
 
 echo 'basedir: '. ini_get('open_basedir') .'<br/>';
-echo 'filesize: ../phpinfo.php - '. filesize('../phpinfo.php') . '<br/>';
+echo 'filesize: ../phpinfo.php - '. filesize(__DIR__ .'/../phpinfo.php') . '<br/>';
--- files/index.php
+++ files/index.php
@@ -1,6 +1,6 @@
 Filesystem
 <ul>
     <li>
-        <a href="files/basedir.php">Apply <code>open_basedir</code> directive</a>
+        <a href="display.php?file=files/basedir">Apply <code>open_basedir</code> directive</a>
     </li>
 </ul>
