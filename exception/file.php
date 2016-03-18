ce8a17b6a4fe177600bdb716ecc0129b44448db1
AntonShevchuk <AntonShevchuk@gmail.com>
2016-03-18 13:11:55 +0200

    Updated exception examples

--- exception/file.php
+++ exception/file.php
@@ -4,18 +4,39 @@ ini_set('display_errors', 1);
 
 $directory = __DIR__ . DIRECTORY_SEPARATOR . 'logs';
 
-if (!is_dir($directory)) {
-    throw new Exception('Directory `logs` is not exists');
+
+class FileSystemException extends Exception {}
+
+class DirectoryException extends FileSystemException {
+    const DIRECTORY_NOT_EXISTS =  1;
+    const DIRECTORY_NOT_WRITABLE = 2;
 }
 
-if (!is_writable($directory)) {
-    throw new Exception('Directory `logs` is not writable');
+class FileException extends FileSystemException {}
+
+
+try {
+    // код который пишет в файл
+    if (!is_dir($directory)) {
+        throw new DirectoryException('Directory `logs` is not exists', DirectoryException::DIRECTORY_NOT_EXISTS);
+    }
+
+    if (!is_writable($directory)) {
+        throw new DirectoryException('Directory `logs` is not writable', DirectoryException::DIRECTORY_NOT_WRITABLE);
+    }
+
+    if (!$file = @fopen($directory . DIRECTORY_SEPARATOR . date('Y-m-d') . '.log', 'a+')) {
+        throw new FileException('System can\'t open log file');
+    }
+
+    fputs($file, date("[H:i:s]") . " done\n");
+    fclose($file);
+} catch (DirectoryException $e) {
+    echo "С директорией возникла проблема: ". $e->getMessage();
+} catch (FileException $e) {
+    echo "С файлом возникла проблема: ". $e->getMessage();
+} catch (FileSystemException $e) {
+    echo "Ошибка файловой системы: ". $e->getMessage();
+} catch (Exception $e) {
+    echo "Ошибка сервера: ". $e->getMessage();
 }
-
-if (!$file = @fopen($directory . DIRECTORY_SEPARATOR . date('Y-m-d') . '.log', 'a+')) {
-    throw new Exception('System can\'t open log file');
-}
-
-fputs($file, date("[H:i:s]") . " done\n");
-
-fclose($file);
