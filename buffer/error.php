8457c6d0c04a0939849a8fca218099e43185adae
Anton <AntonShevchuk@gmail.com>
2016-08-12 14:44:38 +0300

    Updated OB examples

--- buffer/error.php
+++ buffer/error.php
@@ -1,13 +1,18 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
 $buffer = ini_get('output_buffering') ?: 1;
 
-echo str_pad('', $buffer, ' ');
+echo str_pad('', $buffer - 1);
 
-header("PHP-VERSION: ". PHP_VERSION);
+header("TAG-A: ". PHP_VERSION);
 
-echo '<h3>Open browser console and try to find `PHP-VERSION` header</h3>';
+echo ' ';
+
+header("TAG-B: ". PHP_VERSION);
+
+echo '<h3>Open browser console and try to find `TAG-A` and `TAG-B` headers</h3>';
 
 echo 'Output buffer level: ' . ob_get_level() . "<br/>\n";
-echo 'Output buffering: ' . $buffer . "<br/>\n";
+echo 'Output buffering value: ' . $buffer . "<br/>\n";
